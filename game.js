41fc7e9628b5a25ffab9366e0411659a227aaa9b
tsunamham <69695240+tsunamham@users.noreply.github.com>
2020-11-16 15:27:37 -0700

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
+      window.location.href = 'https://tsunamham.github.io/snake_game/'
     }
     return
   }
